1d8ebaf25dddeede0cc236d711c01ed2eccdbb93
hans <hmrtn@pm.me>
2022-04-05 17:32:23 -0700

    added: votes struct

--- models/ballot.go
+++ models/ballot.go
@@ -1,9 +1,16 @@
 package models
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type Ballot struct {
 	ID        string `json:"id,omitempty" bson:"_id,omitempty"`
 	Version   string `json:"ballot_version"`
 	Account   string `json:"ballot_account"`
-	Votes     string `json:"ballot_votes"`
+	Votes     []Vote `json:"ballot_votes"`
 	Signature string `json:"ballot_signature"`
+}
+
+type Vote struct {
+	ID    primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"` // omitempty to protect against zeroed _id insertion
+	Votes string             `json:"votes"`
 }
