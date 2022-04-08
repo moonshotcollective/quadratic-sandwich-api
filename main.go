4018772875e640193ced7db3d643150d8c79f3d2
hans <hmrtn@pm.me>
2022-04-07 17:29:13 -0700

    rm init

--- main.go
+++ main.go
@@ -1,62 +1,15 @@
 package main
 
 import (
-	"context"
 	"log"
 	"os"
 	"quadratic-sandwich-api/routes"
-	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/limiter"
 	"github.com/gofiber/fiber/v2/middleware/logger"
-	"github.com/joho/godotenv"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
-
-type MongoInstance struct {
-	Client *mongo.Client
-	DB     *mongo.Database
-}
-
-var mongoURI string
-var projectCollection string
-var ballotCollection string
-var dbName string
-var apiVersion string
-
-var mg MongoInstance
-
-func init() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Print("Error loading .env file")
-	}
-	// Get env variables
-	mongoURI = os.Getenv("DATABASE_URL")
-	projectCollection = os.Getenv("PROJECT_COLLECTION")
-	ballotCollection = os.Getenv("BALLOT_COLLECTION")
-	dbName = os.Getenv("DATABASE_NAME")
-	apiVersion = os.Getenv("API_VERSION")
-
-	client, err := mongo.NewClient(options.Client().ApplyURI(mongoURI))
-	if err != nil {
-		log.Fatal(err)
-	}
-	ctx, stop := context.WithTimeout(context.Background(), 30*time.Second)
-	defer stop()
-	err = client.Connect(ctx)
-	db := client.Database(dbName)
-	if err != nil {
-		log.Fatal(err)
-	}
-	mg = MongoInstance{
-		Client: client,
-		DB:     db,
-	}
-}
 
 func main() {
 	// Create a new Fiber service
