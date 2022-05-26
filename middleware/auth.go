044068408a2b8c5affbedccf121bb31836535277
hans <hmrtn@pm.me>
2022-05-26 14:02:13 -0700

    added: WIP some metadata to auth method

--- middleware/auth.go
+++ middleware/auth.go
@@ -141,6 +141,75 @@ func hasMinted(account string) bool {
 	return res["0"].(*big.Int).Cmp(big.NewInt(1)) == 0
 }
 
+func getOpCoSupply(account string) *big.Int {
+	var functions = []string{
+		"function getOpCoSupply(address account) public view returns (uint256)",
+	}
+	abiContract, err := abi.NewABIFromList(functions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	addr := ethgo.HexToAddress(contract_address)
+	client, err := jsonrpc.NewClient(rpc_endpoint)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c := contract.NewContract(addr, abiContract, client)
+	res, err := c.Call("getOpCoSupply", ethgo.Latest, account)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return res["0"].(*big.Int)
+}
+
+func getOpCoAllocated(account string) *big.Int {
+	var functions = []string{
+		"function getOpCoAllocated(address account) public view returns (uint256)",
+	}
+	abiContract, err := abi.NewABIFromList(functions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	addr := ethgo.HexToAddress(contract_address)
+	client, err := jsonrpc.NewClient(rpc_endpoint)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c := contract.NewContract(addr, abiContract, client)
+	res, err := c.Call("getOpCoAllocated", ethgo.Latest, account)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return res["0"].(*big.Int)
+}
+
+func getBalanceOf(account string) *big.Int {
+	var functions = []string{
+		"function balanceOf(address account) public view returns (uint256)",
+	}
+	abiContract, err := abi.NewABIFromList(functions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	addr := ethgo.HexToAddress(contract_address)
+	client, err := jsonrpc.NewClient(rpc_endpoint)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c := contract.NewContract(addr, abiContract, client)
+	res, err := c.Call("balanceOf", ethgo.Latest, account)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return res["0"].(*big.Int)
+}
+
 func Login(ctx *fiber.Ctx) error {
 	account := ctx.FormValue("account")
 	signature := ctx.FormValue("signature")
@@ -152,15 +221,29 @@ func Login(ctx *fiber.Ctx) error {
 	// Set Auth Role
 	// NOTE: Can only be one role, and in this heirarchy!
 	// TODO: Deprecate in favor of more explicit claims (below)
-	var role = "public"
+	var role string
+	var meta interface{}
 	if isOp(account) {
 		role = "OP_ROLE"
-	}
-	if isOpCo(account) {
+	} else if isOpCo(account) {
 		role = "OPCO_ROLE"
-	}
-	if isCitizen(account) {
+		meta = struct {
+			supply    *big.Int
+			allocated *big.Int
+		}{
+			supply:    getOpCoSupply(account),
+			allocated: getOpCoAllocated(account),
+		}
+	} else if isCitizen(account) {
 		role = "OPCO_CITIZEN_ROLE"
+		meta = struct {
+			balance *big.Int
+		}{
+			balance: getBalanceOf(account),
+		}
+	} else {
+		role = "public"
+		meta = struct{}{}
 	}
 
 	// Create the Claims
@@ -173,6 +256,7 @@ func Login(ctx *fiber.Ctx) error {
 		"isCitizen": isCitizen(account),
 		"minted":    hasMinted(account),
 		"exp":       time.Now().Add(time.Hour * 6).Unix(),
+		"meta":      meta,
 	}
 	// Create token
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
