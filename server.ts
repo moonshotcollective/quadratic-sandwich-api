107e35a94f05583aada64451c5a1eb728f929348
hans <hmrtn@pm.me>
2022-06-07 20:59:51 -0700

    added: server infra for mongoose and mongodb

--- server.ts
+++ server.ts
@@ -1,19 +1,28 @@
 import express from 'express';
 import { Application } from 'express';
 import * as dotenv from 'dotenv';
-dotenv.config();
+
+const result = dotenv.config();
+if (result.error) {
+    dotenv.config({ path: '.env' });
+}
 
 import Server from './src/api/v1/index';
 import ContractEventService from './src/api/v1/services/ContractEvents.service';
 import { generateJWT } from './src/api/v1/utils/jwt.utils';
+import MongoConnection from './src/api/v1/config/db.config';
 
 // Only generate a token for lower level environments
 if (process.env.NODE_ENV !== 'production') {
-    console.log('JWT', generateJWT());
+    // console.log('JWT', generateJWT());
 }
 
 // Start the Contract Services
 const contractEventService = new ContractEventService();
+
+const dbConnection = new MongoConnection(
+    process.env.MONGO_URI ? process.env.MONGO_URI : '',
+);
 
 // Express API
 const app: Application = express();
@@ -21,12 +30,36 @@ const server: Server = new Server(app);
 const port: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 const host: string = 'localhost';
 
-app.listen(port, host, () => {
-    console.log(`Server started on http://${host}:${port}`);
-}).on('error', (err: any) => {
-    if (err.code === 'EADDRINUSE') {
-        console.log('server startup error: address already in use');
-    } else {
-        console.log(err);
-    }
+dbConnection.connect(() => {
+    app.listen(port, host, () => {
+        console.log({
+            level: 'info',
+            message: `🌏 Express server started on http://${host}:${port}`,
+        });
+    }).on('error', (err: any) => {
+        if (err.code === 'EADDRINUSE') {
+            console.log({
+                level: 'error',
+                message: 'server startup error: address already in use',
+                error: err,
+            });
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+// Close the Mongoose connection, when receiving SIGINT
+process.on('SIGINT', () => {
+    console.info('\nGracefully shutting down');
+    dbConnection.close(err => {
+        if (err) {
+            console.log({
+                level: 'error',
+                message: 'Error shutting closing mongo connection',
+                error: err,
+            });
+        }
+        process.exit(0);
+    });
 });
---
+++ src/api/v1/config/db.config.ts
@@ -0,0 +1,101 @@
+import mongoose from 'mongoose';
+
+// To use global promise for mongoose
+(<any>mongoose).Promise = global.Promise;
+
+export default class MongoConnection {
+    /** URL to access mongo */
+    private readonly mongoUri: string;
+
+    /** Callback when mongo connection is established or re-established */
+    private onConnectedCallback!: IOnConnectedCallback;
+
+    /**
+     * Internal flag to check if connection established for
+     * first time or after a disconnection
+     */
+    private isConnectedBefore: boolean = false;
+
+    /**
+     * Start mongo connection
+     * @param mongoUri MongoDB URL
+     * @param onConnectedCallback callback to be called when mongo connection is successful
+     */
+    constructor(mongoUri: string) {
+        if (process.env.NODE_ENV === 'development') {
+            mongoose.set('debug', true);
+        }
+
+        this.mongoUri = mongoUri;
+        mongoose.connection.on('error', this.onError);
+        mongoose.connection.on('disconnected', this.onDisconnected);
+        mongoose.connection.on('connected', this.onConnected);
+        mongoose.connection.on('reconnected', this.onReconnected);
+    }
+
+    /** Close mongo connection */
+    public close(onClosed: (err: any) => void) {
+        console.log({
+            level: 'info',
+            message: 'Closing the MongoDB connection',
+        });
+        // noinspection JSIgnoredPromiseFromCall
+        mongoose.connection.close(onClosed);
+    }
+
+    /** Start mongo connection */
+    public connect(onConnectedCallback: IOnConnectedCallback) {
+        this.onConnectedCallback = onConnectedCallback;
+        this.startConnection();
+    }
+
+    private startConnection = () => {
+        console.log({
+            level: 'info',
+            message: `Connecting to MongoDB at ${this.mongoUri}`,
+        });
+        mongoose.connect(this.mongoUri).catch(() => {});
+    };
+
+    /**
+     * Handler called when mongo connection is established
+     */
+    private onConnected = () => {
+        console.log({
+            level: 'info',
+            message: `Connected to MongoDB at ${this.mongoUri}`,
+        });
+        this.isConnectedBefore = true;
+        this.onConnectedCallback();
+    };
+
+    /** Handler called when mongo gets re-connected to the database */
+    private onReconnected = () => {
+        console.log({
+            level: 'info',
+            message: 'Reconnected to MongoDB',
+        });
+        this.onConnectedCallback();
+    };
+
+    /** Handler called for mongo connection errors */
+    private onError = () => {
+        console.log({
+            level: 'error',
+            message: `Could not connect to ${this.mongoUri}`,
+        });
+    };
+
+    /** Handler called when mongo connection is lost */
+    private onDisconnected = () => {
+        if (!this.isConnectedBefore) {
+            setTimeout(() => {
+                this.startConnection();
+            }, 2000);
+            console.log({
+                level: 'info',
+                message: 'Retrying mongo connection',
+            });
+        }
+    };
+}
