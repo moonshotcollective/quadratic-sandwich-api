b6592a2fac125f0d17248510834158f9b0b8abc1
hans <hmrtn@pm.me>
2022-06-15 01:29:47 -0600

    yup

--- src/api/v1/models/Citizen.model.ts
+++ src/api/v1/models/Citizen.model.ts
@@ -1,10 +1,11 @@
 import { Model, Schema, model } from 'mongoose';
+import mongooseUniqueValidator from 'mongoose-unique-validator';
 import { ICitizen } from '../interfaces/citizen.i';
 
 interface ICitizenModel extends Model<ICitizen> {}
 
 const citizenSchema = new Schema({
-    address: { type: String, required: true },
+    address: { type: String, required: true, unique: true },
     ens: { type: String },
     opco: { type: String },
     minted: { type: Boolean },
@@ -12,6 +13,8 @@ const citizenSchema = new Schema({
     votes: { type: Object }, // TODO: Define voting interface
     meta: { type: Object }, // TODO: Define citizen metadata interface
 });
+
+citizenSchema.plugin(mongooseUniqueValidator); 
 
 export const Citizen: ICitizenModel = model<ICitizen, ICitizenModel>(
     'citizens',
