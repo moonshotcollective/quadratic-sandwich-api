50c9161265e067c4ec1da644cf9fdc3fd0af8d91
hans <hmrtn@pm.me>
2022-07-11 17:28:59 -0700

    functionized class

--- src/api/v1/routes/index.ts
+++ src/api/v1/routes/index.ts
@@ -4,16 +4,14 @@ import opCoRouter from './opco.routes';
 import citizenRouter from './citizen.routes';
 import loginRouter from './login.routes';
 
-export default class Routes {
-
-  constructor(app: Application) {
-    // Login routes
-    app.use(`/api/${process.env.API_VERSION}/login`, loginRouter);
-    // Op routes
-    app.use(`/api/${process.env.API_VERSION}/op`, opRouter);
-    // OpCo routes
-    app.use(`/api/${process.env.API_VERSION}/opco`, opCoRouter);
-    // Citizen routes
-    app.use(`/api/${process.env.API_VERSION}/citizen`, citizenRouter);
-  }
+export const setupRoutes = (app: Application): Application => {
+      // Login routes
+      app.use(`/api/${process.env.API_VERSION}/login`, loginRouter);
+      // Op routes
+      app.use(`/api/${process.env.API_VERSION}/op`, opRouter);
+      // OpCo routes
+      app.use(`/api/${process.env.API_VERSION}/opco`, opCoRouter);
+      // Citizen routes
+      app.use(`/api/${process.env.API_VERSION}/citizen`, citizenRouter);
+      return app;
 }
