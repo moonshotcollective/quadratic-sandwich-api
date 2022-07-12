fece624e4c072fb09d295fc4889a8087eae01a2b
hans <hmrtn@pm.me>
2022-07-11 17:28:46 -0700

    added: checks for login req

--- src/api/v1/utils/verifyEthLogin.utils.ts
+++ src/api/v1/utils/verifyEthLogin.utils.ts
@@ -1,7 +1,11 @@
+import { link } from '@hapi/joi';
 import { utils } from 'ethers'; 
 import { IEthLoginRequest } from '../interfaces/ethLoginRequest.i';
 
 export const verifyEthLoginRequest = (loginRequest: IEthLoginRequest): boolean => {
     const message = 'Please sign this message to connect to the Optimism Collective.';
-    return loginRequest.address === utils.verifyMessage(message, loginRequest.signature)
+    if (loginRequest?.address && loginRequest?.signature) {
+        return loginRequest.address === utils.verifyMessage(message, loginRequest.signature)
+    } 
+    return false;
 }
