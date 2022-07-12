1fde4d4c16a8a9ebf505804fb55021de7404079b
hans <hmrtn@pm.me>
2022-07-12 09:20:06 -0700

    fix: format

--- tests/post.req.test.ts
+++ tests/post.req.test.ts
@@ -3,8 +3,6 @@ import * as dbHandler from './setup';
 import request from 'supertest';
 import app from '../src/api/v1/app';
 
-
-
 beforeAll(async () => {
     await dbHandler.connect();
 });
@@ -18,53 +16,75 @@ afterEach(async () => {
 });
 
 afterAll(() => {
-    return close(); 
+    return close();
 });
 
-const close = async (): Promise<boolean> => { 
+const close = async (): Promise<boolean> => {
     await dbHandler.closeDatabase();
     return true;
-}; 
+};
 
-
-describe("POST", () => {
+describe('POST', () => {
     describe('/login', () => {
         it('should login with address and signature', async () => {
-            const result = await request(app).post(`/api/${process.env.API_VERSION}/login`).send({
-                address: "0xFe59E676BaB8698c70F01023747f2E27e8A065B9",
-                signature: "0x7b9e0919ab52db03110c223f6ab43edd6fc8998a94c53f4b4250675cc58e97882326e06a354796547e5734d6d3382576f72d0c8c18405417ce79a1e69327328f1b"
-            });
-            expect(result.headers['content-type']).toEqual(expect.stringContaining('json'))
+            const result = await request(app)
+                .post(`/api/${process.env.API_VERSION}/login`)
+                .send({
+                    address: '0xFe59E676BaB8698c70F01023747f2E27e8A065B9',
+                    signature:
+                        '0x7b9e0919ab52db03110c223f6ab43edd6fc8998a94c53f4b4250675cc58e97882326e06a354796547e5734d6d3382576f72d0c8c18405417ce79a1e69327328f1b',
+                });
+            expect(result.headers['content-type']).toEqual(
+                expect.stringContaining('json'),
+            );
             expect(result.statusCode).toBe(200);
             // expect the response to regex match a jwt
-            expect(result.body).toMatch(/^[a-zA-Z0-9\-_]+?\.[a-zA-Z0-9\-_]+?\.([a-zA-Z0-9\-_]+)?$/);
-        })
+            expect(result.body).toMatch(
+                /^[a-zA-Z0-9\-_]+?\.[a-zA-Z0-9\-_]+?\.([a-zA-Z0-9\-_]+)?$/,
+            );
+        });
         it('should fail to login with address and missing signature', async () => {
-            const result = await request(app).post(`/api/${process.env.API_VERSION}/login`).send({
-                address: "0xFe59E676BaB8698c70F01023747f2E27e8A065B9",
-                signature: ""
-            });
-            expect(result.headers['content-type']).toEqual(expect.stringContaining('json'))
+            const result = await request(app)
+                .post(`/api/${process.env.API_VERSION}/login`)
+                .send({
+                    address: '0xFe59E676BaB8698c70F01023747f2E27e8A065B9',
+                    signature: '',
+                });
+            expect(result.headers['content-type']).toEqual(
+                expect.stringContaining('json'),
+            );
             expect(result.statusCode).toBe(401);
-        })
+        });
         it('should fail to login with missing address and missing signature', async () => {
-            const result = await request(app).post(`/api/${process.env.API_VERSION}/login`).send({
-                address: "",
-                signature: ""
-            });
-            expect(result.headers['content-type']).toEqual(expect.stringContaining('json'))
+            const result = await request(app)
+                .post(`/api/${process.env.API_VERSION}/login`)
+                .send({
+                    address: '',
+                    signature: '',
+                });
+            expect(result.headers['content-type']).toEqual(
+                expect.stringContaining('json'),
+            );
             expect(result.statusCode).toBe(401);
-        })
-        
+        });
+
         it('should fail to login with signature and missing address', async () => {
-            const result = await request(app).post(`/api/${process.env.API_VERSION}/login`).send({
-                address: "",
-                signature: "0x7b9e0919ab52db03110c223f6ab43edd6fc8998a94c53f4b4250675cc58e97882326e06a354796547e5734d6d3382576f72d0c8c18405417ce79a1e69327328f1b"
-            });
-            expect(result.headers['content-type']).toEqual(expect.stringContaining('json'))
+            const result = await request(app)
+                .post(`/api/${process.env.API_VERSION}/login`)
+                .send({
+                    address: '',
+                    signature:
+                        '0x7b9e0919ab52db03110c223f6ab43edd6fc8998a94c53f4b4250675cc58e97882326e06a354796547e5734d6d3382576f72d0c8c18405417ce79a1e69327328f1b',
+                });
+            expect(result.headers['content-type']).toEqual(
+                expect.stringContaining('json'),
+            );
             expect(result.statusCode).toBe(401);
-        })
-    })
+        });
+    });
 
+    // test citizens post reqs 
+    // describe('/citizen', () => {
 
+    // });
 });
--- tests/save.db.test.ts
+++ tests/save.db.test.ts
@@ -77,61 +77,62 @@ describe('Save to Database', () => {
         });
     });
 
-
-describe('OPCOs', () => {
-    it('should save an OPCO', async () => {
-        expect.assertions(7);
-        const opco: IOPCO = new OPCO({
-            address: '0x0000000000000000000000000000000000000000',
-            ens: 'test.eth',
-            citizens: [],
-            supply: 999,
-            allocated: 999,
-            minted: 999,
-            onboard: 0,
-            meta: {},
+    describe('OPCOs', () => {
+        it('should save an OPCO', async () => {
+            expect.assertions(7);
+            const opco: IOPCO = new OPCO({
+                address: '0x0000000000000000000000000000000000000000',
+                ens: 'test.eth',
+                citizens: [],
+                supply: 999,
+                allocated: 999,
+                minted: 999,
+                onboard: 0,
+                meta: {},
+            });
+            await opco.save();
+            // find inserted opco
+            const foundOpco = await OPCO.findOne({
+                address: opco.address,
+            }).exec();
+            expect(foundOpco?.address).toEqual(opco.address);
+            expect(foundOpco?.ens).toEqual(opco.ens);
+            expect(foundOpco?.citizens).toEqual(opco.citizens);
+            expect(foundOpco?.supply).toEqual(opco.supply);
+            expect(foundOpco?.allocated).toEqual(opco.allocated);
+            expect(foundOpco?.minted).toEqual(opco.minted);
+            expect(foundOpco?.onboard).toEqual(opco.onboard);
         });
-        await opco.save();
-        // find inserted opco
-        const foundOpco = await OPCO.findOne({ address: opco.address }).exec();
-        expect(foundOpco?.address).toEqual(opco.address);
-        expect(foundOpco?.ens).toEqual(opco.ens);
-        expect(foundOpco?.citizens).toEqual(opco.citizens);
-        expect(foundOpco?.supply).toEqual(opco.supply);
-        expect(foundOpco?.allocated).toEqual(opco.allocated);
-        expect(foundOpco?.minted).toEqual(opco.minted);
-        expect(foundOpco?.onboard).toEqual(opco.onboard);
     });
-});
 
-describe('POST: /opco/add/:address', () => {
-    it('should not save a duplicate OPCO', async () => {
-        const opco: IOPCO = new OPCO({
-            address: '0x0000000000000000000000000000000000000000',
-            ens: 'test.eth',
-            citizens: [],
-            supply: 999,
-            allocated: 999,
-            minted: 999,
-            onboard: 0,
-            meta: {},
-        });
+    describe('POST: /opco/add/:address', () => {
+        it('should not save a duplicate OPCO', async () => {
+            const opco: IOPCO = new OPCO({
+                address: '0x0000000000000000000000000000000000000000',
+                ens: 'test.eth',
+                citizens: [],
+                supply: 999,
+                allocated: 999,
+                minted: 999,
+                onboard: 0,
+                meta: {},
+            });
 
-        const dupOPCO: IOPCO = new OPCO({
-            address: '0x0000000000000000000000000000000000000000',
-            ens: 'nottest!.eth',
-            citizens: [],
-            supply: 666,
-            allocated: 666,
-            minted: 666,
-            onboard: 0,
-            meta: {},
+            const dupOPCO: IOPCO = new OPCO({
+                address: '0x0000000000000000000000000000000000000000',
+                ens: 'nottest!.eth',
+                citizens: [],
+                supply: 666,
+                allocated: 666,
+                minted: 666,
+                onboard: 0,
+                meta: {},
+            });
+            await opco.save();
+            const saveDup = async () => {
+                await dupOPCO.save();
+            };
+            await expect(saveDup()).rejects.toThrow();
         });
-        await opco.save();
-        const saveDup = async () => {
-            await dupOPCO.save();
-        };
-        await expect(saveDup()).rejects.toThrow();
     });
-});
 });
